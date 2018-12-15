16c9ed26af49ea89e6b2e1e0ae5d28ac126d40c0
osoken <osoken.devel@outlook.jp>
2018-12-15 15:53:18 +0900

    update

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 import os
 import json
+import random
+import itertools
 
 from flask import Flask, request, jsonify, render_template
 
@@ -12,6 +14,17 @@ with open(os.path.join(wd, 'x.json')) as fin:
     moc = json.load(fin)
 
 
+la_range = [35.6058333333333, 35.6566666666667]
+lo_range = [138.39, 138.46625]
+sz = 500
+la_coords = [
+    la_range[0] + ((la_range[1] - la_range[0]) / sz) * i for i in range(sz)
+]
+lo_coords = [
+    lo_range[0] + ((lo_range[1] - lo_range[0]) / sz) * i for i in range(sz)
+]
+
+
 @app.route("/")
 def hello():
     return render_template(os.path.join(wd, 'static/index.html'))
@@ -19,7 +32,12 @@ def hello():
 
 @app.route('/api/<int:datestr>')
 def api_datestr(datestr):
-    return jsonify(moc)
+
+    return jsonify([{
+        'latitude': x,
+        'longitude': y,
+        'value': random.uniform(0.0, 0.1)
+    } for x, y in itertools.product(la_coords, lo_coords)])
 
 
 if __name__ == '__main__':
